7d895831c05b558d8f2b75696b8766024969aebb
Pablo Duque <menduke@live.com>
2019-01-09 00:24:40 +0100

    First approach to selecting n groups of parents based on lambda

--- parent_selection.jl
+++ parent_selection.jl
@@ -1,9 +1,18 @@
 module ParentSelection
 
-    function random_parent_selection(population)
+    function random_parent_selection(population, λ)
 
-        half_indices = Set(round.(Int, rand(length(population))))
-        other_half = setdiff(Set(1:length(population)), half_indices)
+        split_indices()
+
+    end
+
+    function split_indices(population_length, λ)
+        multiplier = ceil(Int, (λ * 2) / population_length)
+        total_size = population_length * multiplier
+        group_size = total_size / (λ * 2)
+        all_indices = round.(Int, rand(total_size))
+        groups = reshape(1:total_size, (group_size, div(total_size, group_size)))
+        return groups
 
     end
 
