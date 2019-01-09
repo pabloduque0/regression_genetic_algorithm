65a806bc7b71f4ccca13c58c5ead5d89d8eb7c59
Pablo Duque <menduke@live.com>
2019-01-09 18:17:55 +0100

    Fixed split indices

--- parent_selection.jl
+++ parent_selection.jl
@@ -1,18 +1,19 @@
 module ParentSelection
 
     function random_parent_selection(population, λ)
-
-        split_indices()
-
+        groups_indices = split_indices(length(population), λ)
+        groups =
+        for i in 1:size(group)[1]
+            population[groups[i, :]]
     end
 
     function split_indices(population_length, λ)
         multiplier = ceil(Int, (λ * 2) / population_length)
-        total_size = population_length * multiplier
-        group_size = total_size / (λ * 2)
-        all_indices = round.(Int, rand(total_size))
-        groups = reshape(1:total_size, (group_size, div(total_size, group_size)))
-        return groups
+        total_size = floor(Int, population_length * multiplier)
+        group_size = div(total_size, population_length)
+        all_indices = round.(Int, rand(total_size) * population_length)
+        groups_indices = reshape(all_indices, (group_size, div(total_size, group_size)))
+        return groups_indices
 
     end
 
