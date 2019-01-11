7b214ed1ade2ff4b55604ecda1a9cefee8487eda
Pablo Duque <menduke@live.com>
2019-01-11 21:11:37 +0100

    Added parent selection to runner

--- algorithim_runner.jl
+++ algorithim_runner.jl
@@ -1,7 +1,13 @@
 include("./representation.jl")
-using .Representation
+include("./parent_selection.jl")
+using .Representation, .ParentSelection
 
 population_size = 100
 n_kernels = 4
 σ_initial = 1
-Representation.generate_population(population_size, n_kernels, σ_initial)
+λ_children = 100
+
+population = Representation.generate_population(population_size, n_kernels, σ_initial)
+println(population.members[1])
+
+ParentSelection.random_parent_selection(population.members, λ_children)
