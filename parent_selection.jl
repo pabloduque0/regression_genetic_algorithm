9a7ee19ec301bd026b6eca6596dc5689fca79aa7
Pablo Duque <menduke@live.com>
2019-01-13 15:04:06 +0100

    Fixed parent selection for N parents

--- parent_selection.jl
+++ parent_selection.jl
@@ -1,17 +1,20 @@
 module ParentSelection
-
-    function random_parent_selection(population, λ)
-        groups_indices = split_indices(length(population), λ)
-        groups =
-        for i in 1:size(group)[1]
-            population[groups[i, :]]
+    function random_parent_selection(population, λ, μ_parents, output_type)
+        groups_indices = split_indices(length(population.members), λ, μ_parents)
+        groups = Array{output_type, 1}[]
+        n_groups = ceil(Int, (μ_parents * λ) / length(population.members))
+        for i in 1:n_groups
+            group = population.members[groups_indices[i, :]]
+            push!(groups, group)
+        end
+        return groups
     end
 
-    function split_indices(population_length, λ)
-        multiplier = ceil(Int, (λ * 2) / population_length)
+    function split_indices(population_length, λ, μ_parents)
+        multiplier = ceil(Int, (λ * μ_parents) / population_length)
         total_size = floor(Int, population_length * multiplier)
         group_size = div(total_size, population_length)
-        all_indices = round.(Int, rand(total_size) * population_length)
+        all_indices = ceil.(Int, rand(total_size) * population_length)
         groups_indices = reshape(all_indices, (group_size, div(total_size, group_size)))
         return groups_indices
 
