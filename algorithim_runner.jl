5604ba4d89c190899289b3df274ece25ae349a50
Pablo Duque <menduke@live.com>
2019-01-13 18:58:23 +0100

    Added recombination to runner

--- algorithim_runner.jl
+++ algorithim_runner.jl
@@ -1,13 +1,16 @@
 include("./representation.jl")
 include("./parent_selection.jl")
-using .Representation, .ParentSelection
+include("./recombination.jl")
+
+using .Representation, .ParentSelection, .Recombination
 
 population_size = 100
 n_kernels = 4
 σ_initial = 1
 λ_children = 100
+μ_parents = 2
 
 population = Representation.generate_population(population_size, n_kernels, σ_initial)
-println(population.members[1])
-
-ParentSelection.random_parent_selection(population.members, λ_children)
+parents_groups = ParentSelection.random_parent_selection(population, λ_children,
+                                                        μ_parents, Representation.Organism)
+offspring = Recombination.apply_recombination(parents_groups, μ_parents, Recombination.discrete_recombination)
