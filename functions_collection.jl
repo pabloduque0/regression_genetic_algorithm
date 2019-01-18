c5019eeb941b65be2d062b80ff8bb0791faee095
Pablo Duque <menduke@live.com>
2019-01-18 14:47:46 +0100

    Added 5 functions to test

---
+++ functions_collection.jl
@@ -0,0 +1,23 @@
+module FunctionsCollection
+
+    function function1(x)
+        return 2 * exp.(-2 * (x .- 1).^2) .- exp.(-(x .- 1).^2)
+    end
+
+    function function2(x)
+        return x^(1/2)
+
+    function function3(x)
+        return exp.(-x) * sin.(2 * x)
+    end
+
+    function function4(x)
+        return log.(log.(x))
+    end
+
+    function function5(x)
+        return 6 * exp.(-2 * x) + 2 * sin.(x) - cos.(x)
+    end
+
+
+end # FunctionsCollection
