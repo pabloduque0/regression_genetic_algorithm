86684537509e7764c03b9e577704da40cdc93639
Pablo Duque <menduke@live.com>
2019-01-28 17:38:28 +0100

    Minor fixes and added labels

--- graphing_utilities.jl
+++ graphing_utilities.jl
@@ -3,12 +3,14 @@ module GraphingUtilities
     function plot_average_fitness(fitness_values::Array{Array{Float64, 1}, 1},
                                     num_generations::Int)
         averages = [mean(generation_fitvalues) for generation_fitvalues in fitness_values]
-        plot(1:num_generations, averages, lw=3)
+        plot(1:num_generations, averages, lw=3,
+                xlabel="Generation", ylabel="Error", label="Mean Fitness")
     end
 
-    function plot_max_fitness(fitness_values::Array{Array{Float64, 1}, 1})
-        maxes = [max(generation_fitvalues) for generation_fitvalues in fitness_values]
-        plot(1:num_generations, maxes, lw=3)
+    function plot_max_fitness!(fitness_values::Array{Array{Float64, 1}, 1},
+                                num_generations::Int)
+        minimums = [minimum(generation_fitvalues) for generation_fitvalues in fitness_values]
+        plot!(1:num_generations, minimums, lw=3, label="Max fitness")
     end
 
 end
