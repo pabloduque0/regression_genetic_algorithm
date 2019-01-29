8611030e8aa9c301f32e99ad0a5857431047536a
Pablo Duque <menduke@live.com>
2019-01-29 20:20:15 +0100

    Added new params getting them from json file

--- utils.jl
+++ utils.jl
@@ -1,37 +1,68 @@
 module Utils
+    using JSON, ..Metrics, ..Mutation, ..Recombination, ..FunctionsCollection
     function get_algo_params()
-        file_info = open("/harddrive/home/pablo/Google Drive/UNED/Computacion_Evolutiva/Actividades/Actividad_2/genetic_algo_scratch/algo_inputs.txt") do file
-            read(file, String)
-        end
-        file_info = split(file_info, "\n")
-        num_generations,  population_size, number_genes, variable_range = nothing, nothing, nothing, nothing
-        variable_range_low, variable_range_high = nothing, nothing
-        function_param = nothing
+        file_path = "/harddrive/home/pablo/Google Drive/UNED/Computacion_Evolutiva/Actividades/Actividad_2/regression_genetic_algorithim/algo_inputs.txt"
+        parameters = JSON.parse(read(file_path, String))
+        parameters["error_function"] = select_error_function(parameters["error_function"])
+        parameters["recombination_type"] = select_recombination_type(parameters["recombination_type"])
+        parameters["mutation_type"] = select_mutation_type(parameters["mutation_type"])
+        parameters["evaluate_function"] = select_evaluate_function(parameters["evaluate_function"])
 
-        # Could be more efficient
-        for param in file_info
-            param = split(param, "=")
-            if param[1] == "num_generations"
-                num_generations = parse(Int, param[2])
-            elseif param[1] == "population_size"
-                population_size = parse(Int, param[2])
-            elseif param[1] == "number_genes"
-                number_genes = parse(Int, param[2])
-            elseif param[1] == "variable_range_high"
-                variable_range_high = parse(Int, param[2])
-            elseif param[1] == "variable_range_low"
-                variable_range_low = parse(Int, param[2])
-            elseif param[1] == "function_param"
-                function_param = param[2]
-            end
+        return parameters
+    end
+
+    function select_evaluate_function(evaluate_function)
+        if evaluate_function[end] == '1'
+            return FunctionsCollection.function1
+        elseif evaluate_function[end] == '2'
+            return FunctionsCollection.function2
+        elseif evaluate_function[end] == '3'
+            return FunctionsCollection.function3
+        elseif evaluate_function[end] == '4'
+            return FunctionsCollection.function4
+        elseif evaluate_function[end] == '5'
+            return FunctionsCollection.function5
+        else
+            throw(ArgumentError("Function to evaluate requested not available. Options are:
+                                function1, function2, function3, function4, function5"))
         end
-        variable_range = (variable_range_low, variable_range_high)
-        if num_generations == nothing || population_size == nothing ||
-            number_genes == nothing || variable_range == nothing
-            throw(ArgumentError("Missing parameters in algot_inputs.txt."))
+    end
+
+    function select_error_function(error_function)
+        if error_function == "mean_sqr_error"
+            return Metrics.mean_sqr_error
+        elseif error_function == "mean_absolute_error"
+            return Metrics.mean_absolute_error
+        elseif error_function == "weighted_mean_abs_error"
+            return Metrics.weighted_mean_abs_error
+        else
+            throw(ArgumentError("Error function requested not available. Options are: mean_sqr_error,
+                                mean_absolute_error, weighted_mean_abs_error"))
+        end
+    end
+
+    function select_recombination_type(recombination_type)
+
+        if recombination_type == "intermediary_recombination"
+            Recombination.intermediary_recombination
+        elseif recombination_type == "discrete_recombination"
+            Recombination.discrete_recombination
+        else
+            throw(ArgumentError("Recombination type requested not available. Options are:
+                                intermediary_recombination, discrete_recombination"))
+        end
+    end
+
+    function select_mutation_type(mutation_type)
+        if mutation_type == "uncorr_mutation_onestepsize"
+            return Mutation.uncorr_mutation_onestepsize
+        elseif mutation_type == "uncorr_mutation_n_stepsize"
+            return Mutation.uncorr_mutation_n_stepsize
+        else
+            throw(ArgumentError("Mutation type requested not available. Options are:
+                                uncorr_mutation_onestepsize, uncorr_mutation_n_stepsize"))
         end
 
-        return num_generations,  population_size, number_genes, variable_range, function_param
     end
 
     function generate_linespace(min, max, n_values)
