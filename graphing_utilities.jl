7afa01ec038fd6f3eb32a0af9f8d75b51c2c4b0f
Pablo Duque <menduke@live.com>
2019-02-16 17:00:20 +0100

    Added plotting for hit_ratio

--- graphing_utilities.jl
+++ graphing_utilities.jl
@@ -3,20 +3,35 @@ module GraphingUtilities
     function plot_all(fitness_values::Array{Array{Float64, 1}, 1},
                         num_generations::Int, population::Representation.Population,
                         true_values::Array{Float64, 1}, x_values::Array{Float64, 1},
+                        hit_ratios::Array{Array{Float64, 1}, 1},
+                        population_size::Int64,
                         save_name::Union{Nothing, AbstractString})
 
         error_plot = plot(xlabel="Generation", ylabel="Error")
         plot_average_fitness!(fitness_values, num_generations)
         plot_max_fitness!(fitness_values, num_generations)
         func_plot = plot_both_functions(population, true_values, x_values)
+        hit_ratio_plot = plot_metric(hit_ratios, num_generations, population_size,"Hit Ratio")
         if save_name != nothing
             savefig(error_plot, save_name * "_error.png")
             savefig(func_plot, save_name * "_funcs.png")
+            savefig(hit_ratio_plot, save_name * "_hr.png")
         else
             display(error_plot)
             display(func_plot)
+            display(hit_ratio_plot)
         end
     end
+
+    function plot_metric(metric_values::Array{Array{Float64, 1}, 1},
+                            num_generations::Int, population_size::Int64,
+                            metric_name::AbstractString)
+        averages = [mean(generation_values) for generation_values in metric_values]
+        averages = averages / (population_size + 1)
+        plot(1:num_generations, averages, lw=3,
+                xlabel="Generation", ylabel=metric_name, label=metric_name)
+    end
+
     function plot_average_fitness!(fitness_values::Array{Array{Float64, 1}, 1},
                                     num_generations::Int)
         averages = [mean(generation_fitvalues) for generation_fitvalues in fitness_values]
